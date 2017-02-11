2310e5c202c0fe9ce6b79813d619d66e7ecd9c31
Petr Dobroucky <git@ioudas.net>
2017-02-11 14:33:50 +0100

    Added SQL for environment change

---
+++ Database/Site_alias_change.sql
@@ -0,0 +1,24 @@
+﻿
+--PROD
+update  dbo.portalalias set [HTTPAlias] = 'pensiondobroucky.azurewebsites.net/en-us' 
+where [PortalAliasID] = 2
+update  dbo.portalalias set [HTTPAlias] = 'pensiondobroucky.azurewebsites.net/cs-cz' 
+where [PortalAliasID] = 3
+update  dbo.portalalias set [HTTPAlias] = 'pensiondobroucky.azurewebsites.net/de-de' 
+where [PortalAliasID] = 4
+update  dbo.portalalias set [HTTPAlias] = 'pensiondobroucky.azurewebsites.net' 
+where [PortalAliasID] = 5
+
+--DEBUG
+update  dbo.portalalias set [HTTPAlias] = 'localhost:6333/en-us' 
+where [PortalAliasID] = 2
+update  dbo.portalalias set [HTTPAlias] = 'localhost:6333/cs-cz' 
+where [PortalAliasID] = 3
+update  dbo.portalalias set [HTTPAlias] = 'localhost:6333/de-de' 
+where [PortalAliasID] = 4
+update  dbo.portalalias set [HTTPAlias] = 'localhost:6333' 
+where [PortalAliasID] = 5
+
+
+select * from dbo.portalalias
+
